8bdf98bcf475b00a4ba21e8adaa196d3dd5c223c
“Aaron <github email address>
2021-02-22 18:52:52 -0500

    commit tree d1091ef919063c1b3ce5c481a72f92ff9345db5d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+let names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift`);
+  }
+
+  return names;
+}
